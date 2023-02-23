e55e26d9f322e83650aa5965cb56a900399f67c3
NnikolaN <mortinka2017@gmail.com>
2023-02-23 13:34:33 +0200

    First Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,304 @@
+﻿namespace TestWinGeorgiMitev
+{
+    partial class Information
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxFirstName = new System.Windows.Forms.TextBox();
+            this.textBoxSurname = new System.Windows.Forms.TextBox();
+            this.textBoxLastName = new System.Windows.Forms.TextBox();
+            this.textBoxEGN = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.rdButtonMan = new System.Windows.Forms.RadioButton();
+            this.rdButtonWoman = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cmbBox = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.btnFinalise = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBoxFirstName
+            // 
+            this.textBoxFirstName.Location = new System.Drawing.Point(108, 12);
+            this.textBoxFirstName.Name = "textBoxFirstName";
+            this.textBoxFirstName.Size = new System.Drawing.Size(138, 22);
+            this.textBoxFirstName.TabIndex = 0;
+            // 
+            // textBoxSurname
+            // 
+            this.textBoxSurname.Location = new System.Drawing.Point(108, 52);
+            this.textBoxSurname.Name = "textBoxSurname";
+            this.textBoxSurname.Size = new System.Drawing.Size(138, 22);
+            this.textBoxSurname.TabIndex = 0;
+            // 
+            // textBoxLastName
+            // 
+            this.textBoxLastName.Location = new System.Drawing.Point(108, 94);
+            this.textBoxLastName.Name = "textBoxLastName";
+            this.textBoxLastName.Size = new System.Drawing.Size(138, 22);
+            this.textBoxLastName.TabIndex = 0;
+            // 
+            // textBoxEGN
+            // 
+            this.textBoxEGN.Location = new System.Drawing.Point(129, 148);
+            this.textBoxEGN.MaxLength = 10;
+            this.textBoxEGN.Name = "textBoxEGN";
+            this.textBoxEGN.Size = new System.Drawing.Size(92, 22);
+            this.textBoxEGN.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.Location = new System.Drawing.Point(31, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Име";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label2.Location = new System.Drawing.Point(4, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Презиме";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label3.Location = new System.Drawing.Point(-1, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 25);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Фамилия";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label4.Location = new System.Drawing.Point(52, 144);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 25);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "ЕГН";
+            // 
+            // rdButtonMan
+            // 
+            this.rdButtonMan.AutoSize = true;
+            this.rdButtonMan.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.rdButtonMan.Location = new System.Drawing.Point(18, 33);
+            this.rdButtonMan.Name = "rdButtonMan";
+            this.rdButtonMan.Size = new System.Drawing.Size(78, 29);
+            this.rdButtonMan.TabIndex = 2;
+            this.rdButtonMan.TabStop = true;
+            this.rdButtonMan.Text = "Мъж";
+            this.rdButtonMan.UseVisualStyleBackColor = true;
+            // 
+            // rdButtonWoman
+            // 
+            this.rdButtonWoman.AutoSize = true;
+            this.rdButtonWoman.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.rdButtonWoman.Location = new System.Drawing.Point(18, 67);
+            this.rdButtonWoman.Name = "rdButtonWoman";
+            this.rdButtonWoman.Size = new System.Drawing.Size(84, 29);
+            this.rdButtonWoman.TabIndex = 2;
+            this.rdButtonWoman.TabStop = true;
+            this.rdButtonWoman.Text = "Жена";
+            this.rdButtonWoman.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rdButtonWoman);
+            this.groupBox1.Controls.Add(this.rdButtonMan);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.groupBox1.Location = new System.Drawing.Point(326, 18);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(121, 101);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Пол";
+            // 
+            // cmbBox
+            // 
+            this.cmbBox.FormattingEnabled = true;
+            this.cmbBox.Items.AddRange(new object[] {
+            "Учащ",
+            "Държавен служител",
+            "Работник в частна фирма",
+            "Безработен"});
+            this.cmbBox.Location = new System.Drawing.Point(57, 243);
+            this.cmbBox.Name = "cmbBox";
+            this.cmbBox.Size = new System.Drawing.Size(182, 24);
+            this.cmbBox.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(252, 12);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(20, 25);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "*";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label6.ForeColor = System.Drawing.Color.Red;
+            this.label6.Location = new System.Drawing.Point(252, 94);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(20, 25);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "*";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label7.ForeColor = System.Drawing.Color.Red;
+            this.label7.Location = new System.Drawing.Point(226, 148);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(20, 25);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "*";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label8.Location = new System.Drawing.Point(52, 215);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(172, 25);
+            this.label8.TabIndex = 1;
+            this.label8.Text = "Сфера на работа";
+            // 
+            // btnFinalise
+            // 
+            this.btnFinalise.BackColor = System.Drawing.Color.Lime;
+            this.btnFinalise.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.btnFinalise.Location = new System.Drawing.Point(306, 189);
+            this.btnFinalise.Name = "btnFinalise";
+            this.btnFinalise.Size = new System.Drawing.Size(171, 70);
+            this.btnFinalise.TabIndex = 5;
+            this.btnFinalise.Text = "Потвърди";
+            this.btnFinalise.UseVisualStyleBackColor = false;
+            this.btnFinalise.Click += new System.EventHandler(this.btnFinalise_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label9.ForeColor = System.Drawing.Color.Red;
+            this.label9.Location = new System.Drawing.Point(245, 239);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(20, 25);
+            this.label9.TabIndex = 1;
+            this.label9.Text = "*";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label10.ForeColor = System.Drawing.Color.Red;
+            this.label10.Location = new System.Drawing.Point(245, 243);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(20, 25);
+            this.label10.TabIndex = 1;
+            this.label10.Text = "*";
+            // 
+            // Information
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(508, 307);
+            this.Controls.Add(this.btnFinalise);
+            this.Controls.Add(this.cmbBox);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxEGN);
+            this.Controls.Add(this.textBoxLastName);
+            this.Controls.Add(this.textBoxSurname);
+            this.Controls.Add(this.textBoxFirstName);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Information";
+            this.Text = "Информация";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxFirstName;
+        private System.Windows.Forms.TextBox textBoxSurname;
+        private System.Windows.Forms.TextBox textBoxLastName;
+        private System.Windows.Forms.TextBox textBoxEGN;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RadioButton rdButtonMan;
+        private System.Windows.Forms.RadioButton rdButtonWoman;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox cmbBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button btnFinalise;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestWinGeorgiMitev
+{
+    public partial class Information : Form
+    {
+        public Information()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFinalise_Click(object sender, EventArgs e)
+        {
+            string worksphere = "";
+            string pol = "";
+            string firstname = textBoxFirstName.Text;
+            string lastname = textBoxLastName.Text;
+            string egn = textBoxEGN.Text;
+            if (!string.IsNullOrEmpty(cmbBox.Text))
+            {
+                worksphere = cmbBox.Text;
+            }
+            else
+            {
+                MessageBox.Show("Не си избрал сфера на работа.");
+                cmbBox.Focus();
+                return;
+            }
+            if (rdButtonMan.Checked)
+            {
+                pol = rdButtonMan.Text;
+            }
+            if (rdButtonWoman.Checked)
+            {
+                pol = rdButtonWoman.Text;
+            }
+            if (textBoxFirstName.Text == "")
+            {
+                MessageBox.Show("Не си въвел име.");
+            }
+            if (textBoxLastName.Text == "")
+            {
+                MessageBox.Show("Не си въвел фамилия.");
+            }
+            if (textBoxEGN.Text == "")
+            {
+                MessageBox.Show("Не си въвел EGN.");
+            }
+            DialogResult result = MessageBox.Show($"Име: {firstname} Фамилия: {lastname}, ЕГН: {egn}." + $"Пол: \"{pol}\"" + $"Сфера на работа: {worksphere}", "Потвърждавате ли?", MessageBoxButtons.YesNo);
+        }
+
+        
+    }
+}
